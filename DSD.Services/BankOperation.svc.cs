352084b2bd46115ce7f478e92b595a5c85395eef
Jurguen Zambrano <jurguenzambrano@gmail.com>
2016-10-07 23:41:30 -0500

    Agregar archivos de proyecto.

---
+++ DSD.Services/BankOperation.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DSD.BusinessLogic;
+using DSD.BusinessEntity;
+
+namespace DSD.Services
+{
+    public class BankOperation : IBankOperation
+    {
+        public UsuariosBE getUser(Nullable<Guid> gBanco, string xCodigoUsuario)
+        {
+            UsuariosBE oUsuariosBE = new UsuariosBE();
+            oUsuariosBE = new BankBL().getUser(gBanco, xCodigoUsuario);
+            return oUsuariosBE;
+        }
+        public MovimientosBE refillwallet(Nullable<Guid> gBanco, string xCodigoUsuario, string xOperacionBanco, decimal mMonto)
+        {
+            MovimientosBE oMovimientosBE = new MovimientosBE();
+            oMovimientosBE = new BankBL().refillwallet(gBanco, xCodigoUsuario, xOperacionBanco, mMonto);
+            return oMovimientosBE;
+        }
+    }
+}
